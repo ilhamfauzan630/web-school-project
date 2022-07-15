b2195810b2b4fc294a5ef20a974c5eb86371c259
AzizAzmiJ-A-I <101971763+AzizAzmiJ-A-I@users.noreply.github.com>
2022-07-15 15:05:08 +0700

    file pendukung untuk pendaftaran

---
+++ pendaftaran/proses.php
@@ -0,0 +1,20 @@
+<?php 
+
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$alamat = $_POST['alamat'];
+$nom = $_POST['nom'];
+$mtk = $_POST['mtk'];
+$komentar = $_POST['komentar'];
+
+echo "<head><title>PENDAFTARAN SISWA </title></head>";
+$fp = fopen("listdaftar.txt", "a+");
+fputs($fp, "$nama|$alamat|$email|$nom|$mtk|$komentar\n");
+fclose($fp);
+
+header("location: setelah proses.php")
+
+ ?>
+
+
+
---
+++ pendaftaran/setelah proses.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<h2><center>Terimakasih Telah Mengisi Formulir Pendaftaran</center><br>";
+    echo "<center>Info Selanjutnya Akan Dikirimkan Pada Email Kalian</center></h2><br><br>";
+    echo "<a href='../index.php'>Kembali Ke Menu Awal</a><br>";
+?>
