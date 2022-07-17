8ef2ea98c2d4c811e6975b167a07e65bc50030c4
ilhamfauzan630 <101925259+ilhamfauzan630@users.noreply.github.com>
2022-07-17 08:02:15 +0700

    menghapus index.php dan list.php serta mengubah tampilan input

--- daftar.php
+++ daftar.php
@@ -4,50 +4,74 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     	<link href="../css/carousel.css" rel="stylesheet">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/input.css">
 	</head>
 
 	<body>
-	<hr class="border-primary border-3 opacity-75">
-		<div align="center"><strong><font size="6" face="Courier New, Courier, mono">FORM PENDAFTARAN SISWA</font></strong></div>
+	<header>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0054da;">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/logo.gif" alt="" width="30" class="d-inline-block align-text-top">
+                    SMK DKC
+                </a>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="liststruktur.html">Struktur</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="halaman/fasilitas.html">Fasilitas</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+	<main>
+		<hr class="border-primary border-3 opacity-75">
+		<div align="center" class="text-white"><strong><font size="6" face="Courier New, Courier, mono">FORM PENDAFTARAN SISWA</font></strong></div>
 			<form name="form1" method="post" action="pendaftaran/proses.php">
-				<table width="58%" border="0" align="center">
-					<tr>
-						<td>Nama Lengkap</td>
-						<td><input name="nama" type="text" id="nama"></td>
-					</tr>
-
-					<tr>
-						<td>Alamat</td>
-						<td><input name="alamat" type="text" id="alamat"></td>
-					</tr>
-
-					<tr>
-						<td>E-Mail</td>
-						<td><input name="email" type="text" id="email"></td> 
-					</tr>
-
-					<tr>
-						<td>Nomer Hp</td>
-						<td><input name="nom" type="number" id="nom"></td>
-					</tr>
-
-					<tr>
-						<td>Nilai Matematika</td>
-						<td><input name="nom" type="number" id="mtk"></td>
-					</tr>
-
-					<tr>
-						<td>Komentar</td>
-						<td><textarea name="komentar" id="komentar" placeholder="Alasan atau motivasi anda"></textarea></td> </textarea>
-					</tr>
-
-					<tr>
-						<td></td>
-						<td><input type="submit" name="Submit" value="Kirim"><input type="reset" name="Submit2" value="Batal">
-					</td>
-					</tr>
-				</table>
+			<div class="inputBox">
+				<input type="text" name="nama" id="nama" required>
+				<span>Nama Lengkap</span>
+			</div>
+			<br>
+			<div class="inputBox">
+				<input type="text" name="alamat" id="alamat" required>
+				<span>Alamat</span>
+			</div>
+			<br>
+			<div class="inputBox">
+				<input type="text" name="email" id="email" required>
+				<span>E-mail</span>
+			</div>
+			<br>
+			<div class="inputBox">
+				<input type="number" name="nom" id="nom" required>
+				<span>Nomer Hp</span>
+			</div>
+			<br>
+			<div class="inputBox">
+				<input type="number" name="nom" id="mtk" required>
+				<span>Nilai Matematika</span>
+			</div>
+			<br>
+			<div class="inputBox">
+				<input type="text" name="komentar" id="komentar" required>
+				<span>Komentar</span>
+			</div>
+			<br>
+			<button type="submit" name="submit" class="btn btn-primary btn-lg">Kirim</button>
 			</form>
-			<hr class="border-primary border-3 opacity-75">
+			
+			
+	</main>
 	</body>
 </html>
